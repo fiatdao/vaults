76b1f5a28ab77b6e9080b375783898fee6bc90a9
f1a7da0 <100810861+f1a7da0@users.noreply.github.com>
2022-03-04 15:54:18 +0100

    Initial commit

---
+++ src/test/utils/Hevm.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+abstract contract Hevm {
+    // sets the block timestamp to x
+    function warp(uint256 x) public virtual;
+
+    // sets the block number to x
+    function roll(uint256 x) public virtual;
+
+    // sets the slot loc of contract c to val
+    function store(
+        address c,
+        bytes32 loc,
+        bytes32 val
+    ) public virtual;
+
+    function load(address c, bytes32 loc) public virtual returns (bytes32 val);
+
+    function ffi(string[] calldata) external virtual returns (bytes memory);
+}
